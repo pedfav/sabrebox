3fc15ba0f4374e25d6ec5708a51d112f3feb3445
pedfav <pedfav@gmail.com>
2021-08-02 21:58:45 -0300

    Add day percentage

--- src/informations/weather.py
+++ src/informations/weather.py
@@ -17,7 +17,7 @@ class Weather:
       lcd.write_string(f"Temperature={round(temperature, 2)}")
       lcd.cursor_pos=(1,0)
       lcd.write_string(f"Humidity={round(humidity, 2)}")
-      print(f"Time={now} - Temperature={round(temperature, 2)} - Humidity={round(humidity, 2)}")
+      print(f"Temperature={round(temperature, 2)} - Humidity={round(humidity, 2)}")
     else:
       lcd.write_string("DHT22 not workin")
       print("DHT22 not workin")
