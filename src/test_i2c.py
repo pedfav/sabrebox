53f35ae56445a1667d75c2a0b3d5b04c181b9eff
pedfav <pedfav@gmail.com>
2021-08-15 10:00:09 -0300

    fixes

--- src/test_i2c.py
+++ src/test_i2c.py
@@ -4,7 +4,6 @@ import time
 lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1,
               cols=20, rows=4, dotsize=8,
               charmap='A02',
-              auto_linebreaks=True,
               backlight_enabled=True)
 
 
