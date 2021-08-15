d08471895928252ed0f3aff7a09af32494edd40d
pedfav <pedfav@gmail.com>
2021-08-15 01:59:14 -0300

    fixes

--- src/test_i2c.py
+++ src/test_i2c.py
@@ -1,10 +1,12 @@
 from RPLCD.i2c import CharLCD
 
-lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1,
-              cols=20, rows=4, dotsize=8,
-              charmap='A02',
-              auto_linebreaks=True,
-              backlight_enabled=True)
+# lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1,
+#               cols=20, rows=4, dotsize=8,
+#               charmap='A02',
+#               auto_linebreaks=True,
+#               backlight_enabled=True)
+
+lcd = CharLCD('PCF8574', 0x27)
 
 while(True):
   lcd.clear()
