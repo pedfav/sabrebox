0f50313a34da893720918800f96969d4145b5918
pedfav <pedfav@gmail.com>
2021-08-15 09:58:22 -0300

    fixes

--- src/test_i2c.py
+++ src/test_i2c.py
@@ -9,7 +9,6 @@ lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1,
 
 
 while(True):
-  lcd.clear()
   lcd.write_string("First_line")
   lcd.cursor_pos=(1,0)
   lcd.write_string("Second_line")
